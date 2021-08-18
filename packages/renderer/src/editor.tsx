61c0f120f285fe782f485fe00cd96c992b2df4e2
Nikolay Rademacher <hrrdx33@gmail.com>
2021-08-18 05:44:44 +0200

    feat: add theming and markdown support

--- packages/renderer/src/editor.tsx
+++ packages/renderer/src/editor.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import useCodeMirror from './useCodeMirror'
+import './editor.css'
 
-const Editor: React.FC = () => {
-  return <div className="editor-wrapper">Editor</div>
+interface Props {
+  initialDoc: string
+  onChange: (doc: string) => void
+}
+
+const Editor: React.FC<Props> = props => {
+  const { onChange, initialDoc } = props
+  const handleChange = useCallback(
+    state => onChange(state.doc.toString()),
+    [onChange]
+  )
+  const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+    initialDoc: initialDoc,
+    onChange: handleChange
+  })
+
+  useEffect(() => {
+    if (editorView) {
+      // Do nothing for now
+    }
+  }, [editorView])
+  return <div className="editor-wrapper" ref={refContainer}></div>
 }
 
 export default Editor
